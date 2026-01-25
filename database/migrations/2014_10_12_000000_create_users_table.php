c94060d5492a41f7f0de5e0427f292afe337a064
Abel Calderón <rudy_gamerser@hotmail.com>
2026-01-25 09:04:24 -0500

    upd: User migration - modificar tabla inicial para soporte de nuevos campos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,12 +16,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 150);
+            $table->string('email', 150)->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username', 50)->unique();
             $table->string('password');
+            $table->string('role', 20)->default(User::ROLE_APLICADOR);
+
             $table->rememberToken();
             $table->timestamps();
+            $table->index('role');
         });
     }
 
