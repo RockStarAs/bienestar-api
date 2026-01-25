a7bed37687a3e8c5c6524bb42fd99fd33b965609
Abel Calderón <rudy_gamerser@hotmail.com>
2026-01-25 10:48:19 -0500

    add: Test Model para los tests a aplicar en alumnos

---
+++ app/Models/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    const STATUS_ACTIVE = 'active';
+    const STATUS_CLOSED = 'closed';
+
+    use HasFactory;
+}
