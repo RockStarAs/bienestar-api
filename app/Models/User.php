09f4f67b5feec746efd406f620eec53ca5d200c4
Abel Calderón <rudy_gamerser@hotmail.com>
2026-01-25 12:55:23 -0500

    upd: usar trait HasApiTokens en user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     const ROLE_ADMIN = 'admin';
     const ROLE_APLICADOR = 'aplicador';
     
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
