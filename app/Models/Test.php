451d45f1e3dc3e905afedf86cf759dba18fb9425
Abel Calderón <rudy_gamerser@hotmail.com>
2026-02-01 10:09:42 -0500

    upd: Test - agregar fillable y relaciones

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -11,4 +11,27 @@ class Test extends Model
     const STATUS_CLOSED = 'closed';
 
     use HasFactory;
+
+    protected $fillable = [
+        'template_version_id',
+        'title',
+        'period',
+        'status',
+        'created_by'
+    ];
+
+    public function templateVersion()
+    {
+        return $this->belongsTo(TestTemplateVersion::class, 'template_version_id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function assignments()
+    {
+        return $this->hasMany(TestAssignment::class);
+    }
 }
