3bbae29b0c388fbda307098990037fe2b110f5d4
Abel Calderón <rudy_gamerser@hotmail.com>
2026-02-01 09:54:54 -0500

    add: crear tabla students para los estudiantes

---
+++ database/migrations/2026_02_01_094714_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('dni', 20)->unique();
+            $table->string('name', 150);
+            $table->string('email', 150)->nullable();
+            $table->string('program', 150)->nullable(); // Escuela o Carrera
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
