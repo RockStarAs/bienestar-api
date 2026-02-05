97dde6ce01ddb9114a6b04ae64d24449bda9ade2
Abel Calderón <rudy_gamerser@hotmail.com>
2026-02-05 00:26:45 -0500

    add: Periodos tabla

---
+++ app/Models/Period.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Period extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+
+    /**
+     * Get the tests associated with this period.
+     */
+    public function tests()
+    {
+        return $this->hasMany(Test::class);
+    }
+}
